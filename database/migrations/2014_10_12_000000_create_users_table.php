f4a96433f0c3a4ce8d47a6b905f9f3608c567e47
LauraCamilaPaez <59691866+LauraCamilaPaez@users.noreply.github.com>
2021-05-23 13:00:08 -0500

    Update .gitignore

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('lastnames', 45);
             $table->string('email')->unique();
             $table->string('password', 45);
+            $table->string('gender', 45);
+            $table->string('document', 25);
+            $table->string('phone', 15);
+            $table->string('mobile', 25);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
